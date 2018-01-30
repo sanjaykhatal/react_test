c1fbc51204ee9870c13d566bd1b7c632c600460c
sanjaykhatal <35961836+sanjaykhatal@users.noreply.github.com>
2018-01-30 21:18:16 +0530

    Add files via upload
    react project files

---
+++ video_list_item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+  console.log(video);
+  //const video = props.video;
+  return <li>video</li>
+};
+
+
+export default VideoListItem;
